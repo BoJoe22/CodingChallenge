f633734e0c0d8d52e597b33d3142fa72efeaf342
bojoe22 <josef.a.koza@gmail.com>
2015-03-03 07:58:57 -0500

    Base setup of class and test module

---
+++ ServerIPs/ServerIPs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerIPs
+{
+    public class ServerIPs
+    {
+        public bool Add(string ip)
+        {
+            return false;
+        }
+
+        public bool remove(string ip)
+        {
+            return false;
+        }
+
+        public string getRandomServer()
+        {
+            return null;
+        }
+    }
+}
