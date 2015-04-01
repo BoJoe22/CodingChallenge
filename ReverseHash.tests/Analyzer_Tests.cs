8cee3b685c9558faaea9b1f704228ecc0dd6fc44
jkoza <jkoza@simventions.com>
2015-04-01 15:00:49 -0400

    ReverseHash[FSharp] More failing code

---
+++ ReverseHash.tests/Analyzer_Tests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReverseHash.tests
+{
+    [TestClass]
+    public class Analyzer_Tests
+    {
+        [TestMethod]
+        public void test_findString()
+        {
+            var result = Analyzer.findString(680131659347);
+            Console.WriteLine(string.Format("Result was \"{0}\""), result);
+        }
+
+        [TestMethod]
+        public void test_countPermutations()
+        {
+            var count = Analyzer.countPermutations;
+            Console.WriteLine(string.Format("Number of permutations is {0}"), count);
+
+            Assert.AreEqual(57657600, count);
+        }
+
+        [TestMethod]
+        public void test_getPermsWithRep()
+        {
+            var count = Analyzer.countPermutations2;
+
+            Console.WriteLine(string.Format("Number of permutations is {0}"), count);
+            Assert.AreEqual(57657600, count);
+        }
+    }
+}
--- ReverseHash/Analyzer.fs
+++ ReverseHash/Analyzer.fs
@@ -1,5 +1,7 @@
 ﻿namespace ReverseHash
 
+open System
+
 module Analyzer =
     let letters = [| 'a'; 'c'; 'd'; 'e'; 'g'; 'i'; 'l'; 'm'; 'n'; 'o'; 'p'; 'r'; 's'; 't'; 'u'; 'w' |]
 
@@ -10,6 +12,26 @@ module Analyzer =
 //            else hashAcc(acc * 37 + Array.findIndex(s.[i]), )
 //        for i in 0..s.Length do
             
+    let rec permutationBuilder acc size set = seq {
+        match size, set with
+        | n, x::xs ->
+            if n > 0 then 
+                for item in letters do
+                    yield! permutationBuilder (item::acc) (n - 1) set 
+//            if n > 0 then yield! permutationBuilder (x::acc) (n - 1) xs 
+//            if n >= 0 then yield! permutationBuilder acc n xs  
+        | 0, [] -> yield acc
+        | _, [] -> ()
+    }
+        
+    let rec outerProduct = function
+        | [] -> Seq.singleton []
+        | L::Ls -> L |> Seq.collect (fun x -> outerProduct Ls |> Seq.map(fun L -> x::L))
+
+    let getPermsWithRep n L =
+        List.replicate n L |> outerProduct
+        
+
     let rec combinations acc size set = seq {
         match size, set with
         | n, x::xs ->
@@ -18,8 +40,19 @@ module Analyzer =
         | 0, [] -> yield acc
         | _, [] -> () }
 
-    let permutations = combinations [] 7 (Array.toList letters)
+//    let permutations = combinations [] 7 (Array.toList letters)
+    let permutations = permutationBuilder [] 7 (Array.toList letters)
 
-    let findString hashToFind =
-        permutations
-        |> Seq.find(fun x -> hash(String.concat("") (List.toSeq x)) = hashToFind)
+    let countPermutations = Seq.length permutations
+
+    let countPermutations2 = getPermsWithRep 7 (Array.toList letters) |> Seq.length
+
+    let findString (hashToFind:int64) =
+        let temp = permutations
+        let temp4 = Seq.length temp
+        let temp2 = permutations |> Seq.nth 1
+        let temp3 = int64(hash(new String(Array.ofList(temp2))))
+        let result = permutations
+                     |> Seq.find(fun x -> hashToFind = int64(hash(new String(Array.ofList(x)))))
+                     |> Array.ofList
+        new String(result)
