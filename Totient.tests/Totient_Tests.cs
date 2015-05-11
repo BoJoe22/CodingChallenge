9c6147e4e61d1fd2dcae2378adacec0d639d1d43
bojoe22 <josef.a.koza@gmail.com>
2015-05-11 03:54:12 -0400

    Solution for Totient

---
+++ Totient.tests/Totient_Tests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeChallenge;
+
+namespace CodeChallenge.tests
+{
+    [TestClass]
+    public class Totient_Tests
+    {
+        [TestMethod]
+        public void test_gcd()
+        {
+            var expected = 5;
+            var result = Totient.gcd(1000000, 986455);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void test_Phi_of5is4()
+        {
+            var expected = 4;
+            var result = Totient.Phi(5);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void test_Phi_upTo10()
+        {
+            var expected = new int[] { 1, 2, 2, 4, 2, 6, 4, 6, 4 };
+
+            var first10 = Enumerable.Range(2, 9).ToArray();
+            var phiArray = first10.Select(x => Totient.Phi(x)).ToArray();
+
+            CollectionAssert.AreEquivalent(expected, phiArray);
+        }
+
+        [TestMethod]
+        public void test_NoverPhi_upTo10()
+        {
+            var expected = new double[] { 2, 1.5, 2, 1.25, 3, 7.0/6, 2, 1.5, 2.5 };
+
+            var first10 = Enumerable.Range(2, 9).ToArray();
+            var nOverPhiArray = first10.Select(x => Totient.NoverPhi(x)).ToArray();
+
+            CollectionAssert.AreEquivalent(expected, nOverPhiArray);
+        }
+
+        [TestMethod]
+        public void test_MaxNoverPhi_upTo10()
+        {
+            var expected = 3;
+            var result = Totient.MaxNoverPhi(10);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void test_MaxNoverPhi_upTo1000()
+        {
+            var expected = 4.375;
+            var result = Totient.MaxNoverPhi(1000);
+            //Console.WriteLine(String.Format("Max N is {0}", result));
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void test_MaxNoverPhi_upTo1000000()
+        {
+            //var expected = 4.375;
+            var result = Totient.MaxNoverPhi(100000);
+            Console.WriteLine(String.Format("Max N over Phi is {0}", result));
+
+            //Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void test_MaxPhi_upTo1000000()
+        {
+            var result = Totient.MaxPhi(1000000);
+            Console.WriteLine(String.Format("Max N is {0}", result));
+        }
+    }
+}
---
+++ Totient/Totient.fs
@@ -0,0 +1,39 @@
+﻿namespace CodeChallenge
+
+module Totient =
+    let rec gcd x y =
+        if y = 0 then x
+        else gcd y (x % y)
+
+    let Phi n =
+        let RelativelyPrime n2 =
+            seq {
+                yield 1
+                for i in [2 .. n2] do
+                    if gcd n2 i = 1 then yield i
+            }
+        
+        n
+        |> RelativelyPrime
+        |> Seq.length
+    
+    let NoverPhi n =
+        double n / (double (Phi n))
+
+    let MaxNoverPhi n =
+        [1 .. n]
+        |> List.map(fun x -> NoverPhi x)
+        |> List.max
+
+    let MaxPhi n =
+        let rec sieve = function
+            | (p::xs) -> p :: sieve [ for x in xs do if x % p > 0 then yield x ]
+            | [] -> []
+        let primes = sieve [2..1000]
+        let rec calculate (acc:int, prims:int list, max:int) =
+            let nextP = acc * prims.Head
+            if nextP > max then acc
+            else calculate (nextP, prims.Tail, max)
+
+        calculate (1, primes, n)
+        |> NoverPhi
