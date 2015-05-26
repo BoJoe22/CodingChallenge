f01e0eeca3fb7412856388b7d0a2a67e008bd490
bojoe22 <josef.a.koza@gmail.com>
2015-05-26 07:53:29 -0400

    [StonePiles] Partial progress that doesn't work

---
+++ StonePiles.tests/Pile_Tests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StonePiles;
+
+namespace StonePiles.tests
+{
+    [TestClass]
+    public class Pile_Tests
+    {
+        [TestMethod]
+        public void Test_GetPileTree()
+        {
+            PileTree tree = new PileTree(5);
+            var result = tree.GetPileTree(5);
+        }
+
+        [TestMethod]
+        public void Test_GetPileCombinations()
+        {
+            PileTree tree = new PileTree(5);
+
+            var result = tree.GetPileCombinations(3);
+
+            Assert.AreEqual(1, result.Count);
+        }
+    }
+}
