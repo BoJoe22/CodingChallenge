3b2af31cf9fe0c91534c820e1fae360a4624f55f
jkoza <jkoza@simventions.com>
2015-06-08 09:36:12 -0400

    [FizzBuzz]  F# solution for FizzBuzz Code Golf challenge

---
+++ FizzBuzz/Script.fsx
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+
+// Define your library scripting code here
+
+[1..100]
+|> Seq.map (function
+| x when x%5=0 && x%3=0 -> "FizzBuzz"
+| x when x%3=0 -> "Fizz"
+| x when x%5=0 -> "Buzz"
+| x -> string x)
+|> Seq.iter (printfn "%s")
